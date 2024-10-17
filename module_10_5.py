45f7fe5cd1117a6c4193db446ed63a6958c69c2c
SergeyL88 <Qwerty@3112311>
2024-10-17 17:35:38 +0400

    Многопроцессное программирование

---
+++ module_10_5.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from multiprocessing import Pool
+
+
+def read_info(name) -> list:
+    all_data: list = []
+    for file in name:
+        with open('./file 1.txt', 'r') as file_:
+            while file_.readline():
+                all_data.append(file_.readline())
+
+
+if __name__ == '__main__':
+    file_names = [f'./file {num}.txt' for num in range(1, 5)]
+
+    # start = datetime.now()
+    # read_info(file_names)
+    # stop = datetime.now()
+    # duration = stop - start
+    # print(duration)
+
+    with Pool(processes=4) as pool:
+        start = datetime.now()
+        pool.map(read_info, file_names)
+    stop = datetime.now()
+    duration = stop - start
+    print(duration)
